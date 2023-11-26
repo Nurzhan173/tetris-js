6370dc2a288e08048f1bfc4a0bf2905008508521
nurzhann173 <nurzhann173@gmail.com>
2023-11-27 00:59:37 +0600

    init

---
+++ src/game.js
@@ -0,0 +1,69 @@
+export default class Game {
+  score = 0;
+  lines = 0;
+  level = 0;
+
+  playField = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+
+  activePiece = {
+    x: 0,
+    y: 0,
+    blocks: [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+    ]
+  }
+
+  movePieceLeft() {
+    this.activePiece.x -= 1;
+
+    if(this.isPieceOutOfBounds()) {
+      this.activePiece.x += 1;
+    }
+  }
+
+  movePieceRight() {
+    this.activePiece.x += 1;
+
+    if(this.isPieceOutOfBounds()) {
+      this.activePiece.x -= 1;
+    }
+  }
+
+  movePieceDown() {
+    this.activePiece.y -= 1;
+
+    if(this.isPieceOutOfBounds()) {
+      this.activePiece.y += 1;
+    }
+  }
+
+  isPieceOutOfBounds() {
+    const playField = this.playField;
+    const {x, y} = playField;
+
+    return playField[y] === undefined || playField [x][y] === undefined;
+  }
+}
